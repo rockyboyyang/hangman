942e7ab5dcaf8d234118b5d852f2d54105e15667
Rocky <yangrocky2013@gmail.com>
2021-02-06 18:04:08 -0600

    add word to guess visual

--- hangman.js
+++ hangman.js
@@ -1,6 +1,6 @@
 const words = {
     "NFL Teams": [
-        'vikings', 'packers', 'bears', 'lions', 'rams', 'seahawks', 'niners', 'cardinals', 'cowboys', 'giants', 'eagles', 'washington football team', 'saints', 'bucaneers', 'panthers', 'falcons',
+        'vikings', 'packers', 'bears', 'lions', 'rams', 'seahawks', 'niners', 'cardinals', 'cowboys', 'giants', 'eagles', 'washington', 'saints', 'bucaneers', 'panthers', 'falcons',
         'steelers', 'browns', 'ravens', 'bengals', 'chargers', 'chiefs', 'raiders', 'broncos', 'patriots', 'bills', 'dolphins', 'jets', 'texans', 'titans', 'jaguars', 'colts'
     ],
     "NBA Teams": [
@@ -9,13 +9,13 @@ const words = {
         'wizards', 'nets', 'knicks', 'celtics', 'hornets', 'magic', 'heat', 'hawks', 'raptors', 'bucks'
     ],
     "TV Sitcoms": [
-        'friends', 'seinfeld', 'how i met your mother', 'the office', 'the big bang theory', 'full house', 'scrubs', 'modern family',
-        'two and a half men', 'malcolm in the middle', 'parks and recreation', 'fresh prince of belair', 'home improvement', 'family matters', 'cheers',
+        'friends', 'seinfeld', 'the office', 'the big bang theory', 'full house', 'scrubs', 'modern family',
+        'home improvement', 'family matters', 'cheers',
         'happy days', 'the brady bunch', 'fresh off the boat', 'roseanne', 'the golden girls'
     ],
     "Music Artists/Groups": [
         'eminem', 'katy perry', 'justin bieber', 'future', 'post malone', 'acdc', 'guns n roses', 'metallica',
-        'dj khalid', 'chance the rapper', 'snoop dogg', 'iron maiden', 'nirvana', 'demi lovato', 'billy joel', 'def leopard',
+        'dj khalid', 'snoop dogg', 'iron maiden', 'nirvana', 'demi lovato', 'billy joel', 'def leopard',
         'the beatles', 'maroon five', 'bangtan boys', 'one direction', 'backstreet boys', 'nsync', 'spice girls', 'motorhead',
         'black sabbath', 'selena gomez', 'shawn mendes', 'wiz khalifa', 'drake', 'beyonce', 'kanye west', 'linkin park'
     ]
@@ -42,9 +42,20 @@ window.addEventListener('DOMContentLoaded', event => {
         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     ]
 
-    console.log(getWord())
     let alphabetContainer = document.getElementById('alphabet_container')
-    
+    let wordToGuessContainer = document.getElementById('word_to_guess')
+    // word to guess
+    let wordAndCategory = getWord()
+    let wordToGuess = wordAndCategory[0]
+
+    // For loop will create divs for all letters in word to guess
+    for(let letter of wordToGuess) {
+        let letterDiv = document.createElement('div')
+        if(letter === ' ') letterDiv.setAttribute('class', `letters_of_word_to_guess is_space`)
+        else letterDiv.setAttribute('class', `letters_of_word_to_guess`)
+        wordToGuessContainer.appendChild(letterDiv)
+    }
+
     // This for loop creates all of my alphabet divs instead of creating
     // 26 divs inside my html file.
     for(let letter of alphabet) {
@@ -56,5 +67,15 @@ window.addEventListener('DOMContentLoaded', event => {
         alphabetContainer.appendChild(letterDiv)
     }
 
+    // guess inside input box
+    let guess = document.getElementById('input_container')
+
+    // clickable divs
+    let guessBtn = document.getElementById('guess_word')
+
     
+
+    guessBtn.addEventListener('click', e => {
+        guessedWord = guess.value
+    })
 })
